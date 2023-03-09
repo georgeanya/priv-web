31d4cf308ddda351a170f2c2ed3b2b5aa3b2cedf
George Anya <georgeanya4real@gmail.com>
2023-03-09 21:55:38 +0100

    commit

--- components/form.tsx
+++ components/form.tsx
@@ -50,6 +50,8 @@ const Form = () => {
 
   const [isError, setIsError] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): any => {
     setState({
       user: {
@@ -75,9 +77,10 @@ const Form = () => {
   const setIsErrorFunc = () => {
     setIsError(!isError);
   };
-  const handleSubmit = (event: React.FormEvent<HTMLButtonElement>): any => {
-    event.preventDefault();
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): any => {
+    event.preventDefault();
+    setIsLoadingFunc();
     axios
       .post(url, {
         first_name: state.user.first_name,
@@ -103,6 +106,10 @@ const Form = () => {
       });
   };
 
+  const setIsLoadingFunc = () => {
+    setIsLoading(!isLoading);
+  };
+
   return (
     <div className="max-w-m mx-5 sm:mx-auto mt-17">
       <div className={isSuccess || isError ? "hidden" : "block"}>
@@ -121,7 +128,7 @@ const Form = () => {
             NGN 4,000
           </p>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-2 gap-5">
             <div className="mb-7">
               <label
@@ -211,6 +218,9 @@ const Form = () => {
             required
           >
             <option>Select condition</option>
+            <option disabled selected hidden>
+              Select Condition
+            </option>
             <option value="erectile dysfunction">Erectile dysfunction</option>
             <option value="premature ejaculation">Premature ejaculation</option>
             <option value="vaginal dryness">Vaginal dryness</option>
@@ -237,15 +247,36 @@ const Form = () => {
             </p>
           </div>
           <div className="mb-15 lg:mb-28">
-            <Link href="">
+            {isLoading ? (
               <SustainButton
                 className="self-center text-sm md:text-base font-medium"
                 type="submit"
-                onClick={handleSubmit}
               >
-                Book consultation
+                <p>Book consultation</p>
               </SustainButton>
-            </Link>
+            ) : (
+              <SustainButton
+                className="self-center text-sm md:text-base font-medium disabled:opacity-65"
+                type="submit"
+              >
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#5355AC]"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </SustainButton>
+            )}
           </div>
         </form>
       </div>
