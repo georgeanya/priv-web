add5ed2724522ac44a08fbec3956cf4049f32f71
George Anya <georgeanya4real@gmail.com>
2023-03-23 16:32:27 +0100

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -8,15 +7,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       {/* Google tag (gtag.js) */}
       <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-N835SQ1X9J"
-        ></Script>
-        <Script id="ga-script">
-          {`window.dataLayer = window.dataLayer || [];
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-N835SQ1X9J"
+      ></Script>
+      <Script id="ga-script">
+        {`window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-N835SQ1X9J');`}
-        </Script>
+      </Script>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,19 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-N835SQ1X9J"
+        ></script>
+        <script id="ga-script">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-N835SQ1X9J');`}
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
