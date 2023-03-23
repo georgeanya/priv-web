d0d25d1d81917318ccf70160d14a499d4c8954bc
George Anya <georgeanya4real@gmail.com>
2023-03-23 12:35:16 +0100

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import  Head from "next/head";
+import Head from "next/head";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
-  return <><Head>
-  {/* Google tag (gtag.js) */}
-  <Script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id=G-N835SQ1X9J"
-  ></Script>
-  <Script id="ga-script">
-    {`window.dataLayer = window.dataLayer || [];
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-N835SQ1X9J"
+        ></Script>
+        <Script id="ga-script">
+          {`window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', 'G-N835SQ1X9J');`}
-  </Script>
-</Head><Component {...pageProps} /></>;
+        </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
