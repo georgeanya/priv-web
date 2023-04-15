265f15c04e987f4bed13bcf939cf1fcc398a5ddb
George Anya <georgeanya4real@gmail.com>
2023-04-15 20:52:50 +0100

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,8 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Google tag (gtag.js) */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-N835SQ1X9J"
-      ></Script>
-      <Script id="ga-script">
-        {`window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-N835SQ1X9J');`}
-      </Script>
+      
+      
       <Component {...pageProps} />
     </>
   );
