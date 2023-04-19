1b49788882643d755cbe145acbbbf5ae6a12983a
George Anya <georgeanya4real@gmail.com>
2023-04-19 19:07:19 +0100

    commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,28 +16,7 @@ export default function Document() {
           gtag('js', new Date());
           gtag('config', 'G-N835SQ1X9J');`}
         </script>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11147200607"
-        ></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-11147200607');`}
-        </script>
-        {/* Hotjar Tracking Code for https://privhealth.co */}
-        <script>
-              
-          {`(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3453562,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-        </script>
+        
       </Head>
       <body>
         <Main />
