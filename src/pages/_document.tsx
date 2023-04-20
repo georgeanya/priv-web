5cac5be271090d986504bd13e4f531e61fbd1ae9
George Anya <georgeanya4real@gmail.com>
2023-04-20 09:45:23 +0100

    commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,11 +20,21 @@ export default function Document() {
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-11147200607"
         ></script>
-        <script  id="ga-script">
+        <script id="ga-script">
           {` window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag(\'js\', new Date());
           gtag(\'config\', \'AW-11147200607\');`}
+        </script>
+        <script>
+          {`(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3453562,hjsv:6};
+            a=o.getElementsByTagName(\'head\')[0];
+            r=o.createElement(\'script\');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,\'https://static.hotjar.com/c/hotjar-\',\'.js?sv=\');`}
         </script>
       </Head>
       <body>
