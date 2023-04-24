cfe548753716b968e64d51cdfec964089bceed49
George Anya <georgeanya4real@gmail.com>
2023-04-24 10:56:47 +0100

    commit

--- src/pages/ed-assessment.tsx
+++ src/pages/ed-assessment.tsx
@@ -94,7 +94,6 @@ const RiskTest = () => {
     if (currentQuestionIndex === questions.length - 1) {
       console.log(`Total score: ${totalScore}`);
       setTestComplete(!testComplete);
-
     } else {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     }
@@ -108,89 +107,86 @@ const RiskTest = () => {
   return (
     <div>
       <Head>
-          <meta charSet="utf-8" />
-          <title>Erectile dysfunction self-assessment - Priv Health</title>
-          <meta name="generator" content="SEOmatic" />
-          <link href={favicon.src} rel="shortcut icon" type="image/png" />
-          <meta
-            name="keywords"
-            content="erectile dysfunction, premature ejaculation, hair loss, low testosterone, genital herpes, male infertility"
-          />
-          <meta name="referrer" content="no-referrer-when-downgrade" />
-          <meta name="robots" content="all" />
-          <meta content="en_US" property="og:locale" />
-          <meta content="website" property="og:type" />
-          <meta content={metaCard.src} property="og:image" />
-          <meta content="1024" property="og:image:width" />
-          <meta content="512" property="og:image:height" />
-          <meta content="An image of the Priv logo" property="og:image:alt" />
-          <meta
-            content="https://facebook.com/tryprivhealth/"
-            property="og:see_also"
-          />
-          <meta
-            content="https://twitter.com/tryprivhealth"
-            property="og:see_also"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@tryprivhealth" />
-          <meta name="twitter:creator" content="@tryprivhealth" />
-          <meta name="twitter:image" content={metaCard.src} />
-          <meta name="twitter:image:width" content="1024" />
-          <meta name="twitter:image:height" content="512" />
-          <meta name="twitter:image:alt" content="An image of the Priv logo" />
-          <link rel="me" href="https://twitter.com/tryprivhealth" />
-          <link href="https://privhealth.co/" rel="home" />
-          <link href="/humans.txt" rel="author" type="text/plain" />
-          <link
-            href="https://privhealth.co/"
-            hrefLang="x-default"
-            rel="alternate"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"
-          />
-          <meta
-            name="facebook-domain-verification"
-            content="ydhokuda2jbyn329ymapza2hdhbumm"
-          />
-          <meta
-            name="description"
-            content="Take this free assessment to know if you may have erectile dysfunction."
-          />
-          <meta content="598084287257839" property="fb:profile_id" />
-          <meta
-            content="https://privhealth.co/ed-assessment"
-            property="og:url"
-          />
-          <meta
-            content="Erectile dysfunction self-assessment - Priv Health"
-            property="og:title"
-          />
-          <meta
-            content="Take this free assessment to know if you may have erectile dysfunction."
-            property="og:description"
-          />
-          <meta
-            content="https://instagram.com/tryprivhealth"
-            property="og:see_also"
-          />
-          <meta
-            name="twitter:title"
-            content="Erectile dysfunction self-assessment - Priv Health"
-          />
-          <meta
-            name="twitter:description"
-            content="Take this free assessment to know if you may have erectile dysfunction."
-          />
-          <link href="https://privhealth.co/ed-assessment" rel="canonical" />
+        <meta charSet="utf-8" />
+        <title>Erectile dysfunction self-assessment - Priv Health</title>
+        <meta name="generator" content="SEOmatic" />
+        <link href={favicon.src} rel="shortcut icon" type="image/png" />
+        <meta
+          name="keywords"
+          content="erectile dysfunction, premature ejaculation, hair loss, low testosterone, genital herpes, male infertility"
+        />
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+        <meta name="robots" content="all" />
+        <meta content="en_US" property="og:locale" />
+        <meta content="website" property="og:type" />
+        <meta content={metaCard.src} property="og:image" />
+        <meta content="1024" property="og:image:width" />
+        <meta content="512" property="og:image:height" />
+        <meta content="An image of the Priv logo" property="og:image:alt" />
+        <meta
+          content="https://facebook.com/tryprivhealth/"
+          property="og:see_also"
+        />
+        <meta
+          content="https://twitter.com/tryprivhealth"
+          property="og:see_also"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@tryprivhealth" />
+        <meta name="twitter:creator" content="@tryprivhealth" />
+        <meta name="twitter:image" content={metaCard.src} />
+        <meta name="twitter:image:width" content="1024" />
+        <meta name="twitter:image:height" content="512" />
+        <meta name="twitter:image:alt" content="An image of the Priv logo" />
+        <link rel="me" href="https://twitter.com/tryprivhealth" />
+        <link href="https://privhealth.co/" rel="home" />
+        <link href="/humans.txt" rel="author" type="text/plain" />
+        <link
+          href="https://privhealth.co/"
+          hrefLang="x-default"
+          rel="alternate"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"
+        />
+        <meta
+          name="facebook-domain-verification"
+          content="ydhokuda2jbyn329ymapza2hdhbumm"
+        />
+        <meta
+          name="description"
+          content="Take this free assessment to know if you may have erectile dysfunction."
+        />
+        <meta content="598084287257839" property="fb:profile_id" />
+        <meta content="https://privhealth.co/ed-assessment" property="og:url" />
+        <meta
+          content="Erectile dysfunction self-assessment - Priv Health"
+          property="og:title"
+        />
+        <meta
+          content="Take this free assessment to know if you may have erectile dysfunction."
+          property="og:description"
+        />
+        <meta
+          content="https://instagram.com/tryprivhealth"
+          property="og:see_also"
+        />
+        <meta
+          name="twitter:title"
+          content="Erectile dysfunction self-assessment - Priv Health"
+        />
+        <meta
+          name="twitter:description"
+          content="Take this free assessment to know if you may have erectile dysfunction."
+        />
+        <link href="https://privhealth.co/ed-assessment" rel="canonical" />
       </Head>
       <Navbar />
       {testComplete ? (
         <div>
           {testStart ? (
-            <div className="md:px-[510px] px-5 mt-[65px] md:mt-[100px] text-center">
+            <div className="md:px-[510px] px-5 mt-[65px] md:mt-[70px] text-center">
               <p className="md:text-[44px] text-3xl leading-[38px] md:leading-[55px] font-bold text-[#5355AC]">
                 Erectile dysfunction self-assessment
               </p>
--- src/pages/ed.tsx
+++ src/pages/ed.tsx
@@ -1,14 +1,93 @@
-import React from 'react'
+import React from "react";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
 import Section4 from "../../components/section4";
 import Section5 from "../../components/section5";
 import Faq from "../../components/faq";
-import Ed from '../../components/ed';
+import Ed from "../../components/ed";
+import Head from "next/head";
+import favicon from "../../public/assets/favicon.png";
+import metaCard from "../../public/assets/priv-metacard.png";
 
 const EdPage = () => {
   return (
     <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Erectile dysfunction (ED) treatment - Priv Health</title>
+        <meta name="generator" content="SEOmatic" />
+        <link href={favicon.src} rel="shortcut icon" type="image/png" />
+        <meta
+          name="keywords"
+          content="erectile dysfunction, premature ejaculation, hair loss, low testosterone, genital herpes, male infertility"
+        />
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+        <meta name="robots" content="all" />
+        <meta content="en_US" property="og:locale" />
+        <meta content="website" property="og:type" />
+        <meta content={metaCard.src} property="og:image" />
+        <meta content="1024" property="og:image:width" />
+        <meta content="512" property="og:image:height" />
+        <meta content="An image of the Priv logo" property="og:image:alt" />
+        <meta
+          content="https://facebook.com/tryprivhealth/"
+          property="og:see_also"
+        />
+        <meta
+          content="https://twitter.com/tryprivhealth"
+          property="og:see_also"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@tryprivhealth" />
+        <meta name="twitter:creator" content="@tryprivhealth" />
+        <meta name="twitter:image" content={metaCard.src} />
+        <meta name="twitter:image:width" content="1024" />
+        <meta name="twitter:image:height" content="512" />
+        <meta name="twitter:image:alt" content="An image of the Priv logo" />
+        <link rel="me" href="https://twitter.com/tryprivhealth" />
+        <link href="https://privhealth.co/" rel="home" />
+        <link href="/humans.txt" rel="author" type="text/plain" />
+        <link
+          href="https://privhealth.co/"
+          hrefLang="x-default"
+          rel="alternate"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1"
+        />
+        <meta
+          name="facebook-domain-verification"
+          content="ydhokuda2jbyn329ymapza2hdhbumm"
+        />
+        <meta
+          name="description"
+          content="Get treatment for erectile dysfunction privately and from the comfort of your home."
+        />
+        <meta content="598084287257839" property="fb:profile_id" />
+        <meta content="https://privhealth.co/ed" property="og:url" />
+        <meta
+          content="Erectile dysfunction (ED) treatment - Priv Health"
+          property="og:title"
+        />
+        <meta
+          content="Get treatment for erectile dysfunction privately and from the comfort of your home."
+          property="og:description"
+        />
+        <meta
+          content="https://instagram.com/tryprivhealth"
+          property="og:see_also"
+        />
+        <meta
+          name="twitter:title"
+          content="Erectile dysfunction (ED) treatment - Priv Health"
+        />
+        <meta
+          name="twitter:description"
+          content="Get treatment for erectile dysfunction privately and from the comfort of your home."
+        />
+        <link href="https://privhealth.co/ed" rel="canonical" />
+      </Head>
       <Navbar />
       <Ed />
       <Section4 />
@@ -16,7 +95,7 @@ const EdPage = () => {
       <Faq />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default EdPage
+export default EdPage;
