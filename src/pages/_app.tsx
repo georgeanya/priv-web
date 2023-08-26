c8303a1c3d5d26a994d1bbbf6a067f27484e338a
George Anya <georgeanya4real@gmail.com>
2023-08-26 22:14:58 +0100

    blog loading bug fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,34 +5,33 @@ import type { AppProps } from "next/app";
 import Spinner from "../../components/spinner";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+  // const router = useRouter();
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const handleStart = (url: any) => {
-      if (url !== router.asPath) {
-        setIsLoading(true);
-      }
-    };
+  // useEffect(() => {
+  //   const handleStart = (url: any) => {
+  //     if (url !== router.asPath) {
+  //       setIsLoading(true);
+  //     }
+  //   };
 
-    const handleComplete = () => {
-      setIsLoading(false);
-    };
+  //   const handleComplete = () => {
+  //     setIsLoading(false);
+  //   };
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
+  //   router.events.on("routeChangeStart", handleStart);
+  //   router.events.on("routeChangeComplete", handleComplete);
+  //   router.events.on("routeChangeError", handleComplete);
 
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  }, [router]);
+  //   return () => {
+  //     router.events.off("routeChangeStart", handleStart);
+  //     router.events.off("routeChangeComplete", handleComplete);
+  //     router.events.off("routeChangeError", handleComplete);
+  //   };
+  // }, [router]);
 
   return (
     <>
-      {isLoading && <Spinner />}
       <Component {...pageProps} />
     </>
   );
