5d356dc97f8ed21f511282d5b1df16a758edb5a3
George Anya <georgeanya4real@gmail.com>
2024-09-14 19:01:41 +0100

    commit

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -27,9 +27,11 @@ const Faq = () => {
   const [faqs, setFaqs] = useState([
     {
       question: "What is Priv?",
-      answer: "Priv is a telehealth provider that offers men accessible and affordable solutions for common health concerns like erectile dysfunction, premature ejaculation, hair loss, and more. Through a combination of licensed doctors, prescription medications, and over-the-counter treatments, Priv provides a modern approach to men's healthcare.",
+      answer:
+        "Priv is a telehealth provider that offers men accessible and affordable solutions for common health concerns like erectile dysfunction, premature ejaculation, hair loss, and more. Through a combination of licensed doctors, prescription medications, and over-the-counter treatments, Priv provides a modern approach to men's healthcare.",
       open: true,
-    },{
+    },
+    {
       question: "Who is Priv for?",
       answer:
         "Priv is designed for men who want convenient, discreet access to healthcare services. Whether you're dealing with sexual health, hair loss concerns or just looking to be healthy, Priv is for those looking for reliable and easy-to-access treatments without the hassle of traditional in-person visits.",
@@ -43,7 +45,8 @@ const Faq = () => {
     },
     {
       question: "Where is Priv available",
-      answer: "Priv is currently available in Nigeria. We aims to provide services in as many regions as possible in the future while adhering to regional healthcare laws.",
+      answer:
+        "Priv is currently available in Nigeria. We aims to provide services in as many regions as possible in the future while adhering to regional healthcare laws.",
       open: false,
     },
     {
@@ -54,9 +57,11 @@ const Faq = () => {
     },
     {
       question: "Can I be accurately assessed remotely?",
-      answer: "Yes, Priv uses a thorough online assessment and remote consultation process to ensure you receive the right care. By answering detailed questions about your health and symptoms, a licensed doctor can assess your condition and recommend appropriate treatment. In some cases, additional information or follow-up may be required for an accurate diagnosis.",
+      answer:
+        "Yes, Priv uses a thorough online assessment and remote consultation process to ensure you receive the right care. By answering detailed questions about your health and symptoms, a licensed doctor can assess your condition and recommend appropriate treatment. In some cases, additional information or follow-up may be required for an accurate diagnosis.",
       open: false,
-    },{
+    },
+    {
       question: "Is my information private and secure?",
       answer:
         "Yes, Priv takes your privacy and security seriously. All personal information is encrypted and stored in compliance with local regulations, ensuring that your medical and personal data is kept confidential and secure.",
@@ -68,7 +73,6 @@ const Faq = () => {
         "Absolutely. All Priv shipments arrive in a nondescript blister package so you can have your treatment shipped wherever makes sense for you as long as there is someone there to sign for it.",
       open: false,
     },
-  
   ]);
 
   const toggleFAQ = (index: number) => {
@@ -170,6 +174,130 @@ const Faq = () => {
           name="facebook-domain-verification"
           content="ydhokuda2jbyn329ymapza2hdhbumm"
         />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "FAQPage",
+              mainEntity: [
+                {
+                  "@type": "Question",
+                  name: "What is Custodia?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Custodia is a digital health program designed to help people reverse chronic metabolic diseases, such as type 2 diabetes, through sustainable lifestyle changes. It offers virtual care, personalized coaching, and medical supervision to address the root causes of these conditions.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "How does Custodia work?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Custodia works by providing personalized health plans that include dietary changes, continuous monitoring, and support from health coaches and doctors. The program focuses on reducing reliance on medications and promoting sustainable lifestyle changes to reverse chronic conditions.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "How much does Custodia cost?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Custodia offers two membership plans: a Comprehensive plan at ₦180,000 biannually and a Basic plan at ₦40,000 per quarter. Both plans include health coaching, doctor supervision, meal plans, and other resources, but the Comprehensive plan also covers routine lab tests and unlimited health supplies.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "What do I get when I join Custodia?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Upon joining Custodia, you receive access to a personal health coach, medical supervision, unlimited consultations, meal plans, resources, and, depending on your plan, routine lab tests and health supplies.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "Can type 2 diabetes be reversed?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Yes, a lot of scientific studies have confirmed that through diet changes and weight loss, the process underlying type 2 diabetes can be put into remission without the help of medication.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "Are medications included in the program?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "No, there are no medications included in the treatment. The goal of the program is to reduce or eliminate your reliance on medication.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "How is diabetes reversal measured?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Type 2 diabetes reversal is officially in remission when HbA1c levels drop to below 6.5% (48mmol/mol) and stay there for 3 months without the help of medication.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "How long does the Custodia program last?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "The duration of the Custodia program can vary based on individual needs and goals, but it is generally designed for ongoing, long-term support until the desired health outcomes are achieved.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "How do I join Custodia?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "To join Custodia, you need to fill out an enrollment form, share your medical history, and make the payment. The program is managed through the Custodia Health app, where you will interact with your care team and track your progress.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "I don’t have a smartphone. Can I still join Custodia?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "The program primarily operates through a mobile app, so a smartphone is essential for participating in the Custodia program effectively.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "Do I have to visit a physical facility for the program?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "No, Custodia is a fully virtual program. All consultations, coaching, and monitoring are done remotely through the app.",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "Can I join Custodia from outside Nigeria?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "Yes, you can. As long as you have a smartphone with internet access you can be a part of the program. If you are having difficulties joining the program, please send us an email at hi@custodiahealth.com",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "Are there any additional costs for lab tests or medical devices?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "For those on the Comprehensive plan, routine lab tests and health supplies are included. However, the Basic plan does not cover these, so there may be additional costs depending on your needs",
+                  },
+                },
+                {
+                  "@type": "Question",
+                  name: "What happens if I need to cancel my enrollment?",
+                  acceptedAnswer: {
+                    "@type": "Answer",
+                    text: "We really think you'll like our program, however if you would like to cancel your enrollment for any reason, simply send us an email at hi@custodiahealth.com",
+                  },
+                },
+              ],
+            }),
+          }}
+        />
       </Head>
       <Navbar />
       <div className="px-5 md:pl-[290px] md:pr-[380px] md:mt-24 mt-16 md:pb-36 pb-24">
@@ -177,7 +305,7 @@ const Faq = () => {
           Frequently Asked Questions
         </h1>
         <p className="mt-5 md:mt-6 text-base md:text-start mb-7 md:mb-14 md:text-[20px] md:leading-[30px] text-[#61616B]">
-        Find answers to commonly asked questions about our telehealth service
+          Find answers to commonly asked questions about our telehealth service
         </p>
         <div className="max-w-3xl mt-17 md:mt-18">
           {faqs.map((faq, index) => (
