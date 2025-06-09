3de75b7535b794abcfc98cc99df9feced5ee8e88
George Anya <georgeanya4real@gmail.com>
2025-06-09 22:29:48 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/barber',
+        destination: 'https://lifebox-labs.notion.site/Priv-partner-program-1aed5793dacc802fbeb3de2d45d3e387',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
