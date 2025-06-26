4508b4c459873a6f2077f6d30f9a065b42028c22
George Anya <georgeanya4real@gmail.com>
2025-06-26 11:09:45 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
         destination: 'https://lifebox-labs.notion.site/Priv-partner-program-1aed5793dacc802fbeb3de2d45d3e387',
         permanent: true,
       },
+      {
+        source: '/start',
+        destination: '/sign-up',
+        permanent: true,
+      },
     ]
   },
 }
